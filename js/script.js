2877fdd2b37804e8233836b9470a4d75b9a7bca8
Glenda <glenda.lborges2@gmail.com>
2022-05-27 20:41:55 -0300

    add section service

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
-$(document).ready(function () {
-    $(".menu-mobile").click(function (){
-        $(this).toggleClass("active");
-        $(".nav").toggleClass("active");
-    });
-});
+let menuMobile = document.getElementById("mobile-menu");
+
+//função para quando o nav estiver a classe ativo o menu vai aparecer
+function toggleMenu() {
+    //chamo pelo o id o nav
+    let nav = document.getElementById('nav');
+    //adiciono uma classe para o nav
+    nav.classList.toggle('active');
+    console.log("ok")
+}
+menuMobile.addEventListener('click', toggleMenu);
